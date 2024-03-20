0154a5e7f27ddcc86e6a9d4ea210646b8dcefebe
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-03-20 15:50:28 +0530

    random tile function for initial move completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 import Grid from "./grid.js";
+import Tile from "./Tile.js";
 
 const gameBoard = document.getElementById('gameBoard')
 
 const grid = new Grid(gameBoard)
+console.log(grid.randomEmptyCell())
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
