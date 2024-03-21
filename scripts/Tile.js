62f0c279c2294f0e167eef95de8c04bf34cf3087
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-03-21 12:24:27 +0530

    Tile.js contains overall functionallity of Tile creation and background changing with relative to number change. Completed the merging function of two tiles

--- scripts/Tile.js
+++ scripts/Tile.js
@@ -3,18 +3,23 @@ export default class Tile{
     #x
     #y
     #value
-
+    
     constructor(tileContainer, value = Math.random() > .5 ? 2 : 4 ) {
         this.#tileElement = document.createElement("div")
         this.#tileElement.classList.add("tile")
         tileContainer.append(this.#tileElement)
         this.value = value
     }
+    
+    get value() {
+        return this.#value
+    }
+
     set value(v){
         this.#value = v 
         this.#tileElement.textContent = v
-        const power = Math.log(v)
-        const backgroundLightness = 100 - power*9
+        const power = Math.log2(v)
+        const backgroundLightness = 100 - power * 9
         this.#tileElement.style.setProperty(
             "--background-lightness",
             `${backgroundLightness}%`
@@ -33,4 +38,7 @@ export default class Tile{
         this.#tileElement.style.setProperty("--y", value)
     }
 
+    remove() {
+        this.#tileElement.remove()
+    }    
 }
--- scripts/grid.js
+++ scripts/grid.js
@@ -10,27 +10,48 @@ export default class Grid {
     #cells
 
     constructor(gridElement) {
-        gridElement.style.setProperty("--grid-size", `${GRID_SIZE}`)
+        gridElement.style.setProperty("--grid-size", GRID_SIZE)
         gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
         gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
-        this.#cells = createCellElement(gridElement).map( (cellElement, index)=>{
+        this.#cells = createCellElement(gridElement).map((cellElement, index) => {
             return new Cell(
-                cellElement, 
-                index % GRID_SIZE, 
-                Math.floor(index / GRID_SIZE) 
+                cellElement,
+                index % GRID_SIZE,
+                Math.floor(index / GRID_SIZE)
             )
         })
         // console.log(this.cells)
     }
 
-    get #emptyCells(){
+    get cells() {
+        return this.#cells
+    }
+
+    get cellsByRow() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.y] = cellGrid[cell.y] || []
+            cellGrid[cell.y][cell.x] = cell
+            return cellGrid
+        }, [])
+    }
+
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || []
+            cellGrid[cell.x][cell.y] = cell
+            return cellGrid
+        }, [])
+    }
+
+    get #emptyCells() {
         return this.#cells.filter(cell => cell.tile == null)
     }
 
-    randomEmptyCell(){
+    randomEmptyCell() {
         const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
         return this.#emptyCells[randomIndex]
     }
+
 }
 
 class Cell {
@@ -38,26 +59,61 @@ class Cell {
     #x
     #y
     #tile
-    constructor(cellElement, x, y){
+    #mergeTile
+
+    constructor(cellElement, x, y) {
         this.#cellElement = cellElement
         this.#x = x
         this.#y = y
     }
 
-    get tile(){
+    get x() {
+        return this.#x
+    }
+    get y() {
+        return this.#y
+    }
+
+    get tile() {
         return this.#tile
     }
-    set tile(value){
+    set tile(value) {
         this.#tile = value
-        if(value == null) return
+        if (value == null) return
         this.#tile.x = this.#x
         this.#tile.y = this.#y
     }
+
+    get mergeTile() {
+        return this.#mergeTile
+    }   
+
+    set mergeTile(value) {
+        this.#mergeTile = value
+        if (value == null) return
+        this.#mergeTile.x = this.#x
+        this.#mergeTile.y = this.#y
+    }
+
+    canAccept(tile) {
+        return (
+            this.tile == null ||
+            (this.mergeTile == null && this.tile.value === tile.value)
+        )
+    }
+
+    mergeTiles() {
+        if (this.tile == null || this.mergeTile == null) return
+        this.tile.value = this.tile.value + this.mergeTile.value
+        this.mergeTile.remove()
+        this.mergeTile = null
+
+    }
 }
 
-function createCellElement(gridElement){
+function createCellElement(gridElement) {
     const cells = []
-    for(let i=0 ; i < GRID_SIZE*GRID_SIZE; i++) {
+    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
         const cell = document.createElement("div")
         cell.classList.add("cell")
         cells.push(cell)
